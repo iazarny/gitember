709163ff258ec7e69b0ea91654a28eac069e8752
Igor Azarny <Igor_Azarny@epam.com>
2016-11-16 12:09:28 +0100

    GA-0000 Initial commit

---
+++ src/main/java/com/az/gitember/FXMLController.java
@@ -0,0 +1,33 @@
+package com.az.gitember;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+public class FXMLController implements Initializable {
+    
+    /*@FXML
+    private Label label;
+    
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+        System.out.println("You clicked me!");
+        label.setText("Hello World!");
+    } */
+
+    @FXML
+    public void exitActionHandler(ActionEvent actionEvent) {
+
+        System.exit(0);
+
+    }
+
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+}
