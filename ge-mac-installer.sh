fb6f0d6585a880db992303dd58812f26f9681d33
ec2-user <ec2-user@ip-172-31-3-78.ec2.internal>
2021-06-08 12:30:03 +0000

    Add shel to build mac pkg

---
+++ ge-mac-installer.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+jpackage --input shade/  --name Gitember2 --vendor "Igor Azarny"  --main-jar gitember.jar  --main-class com.az.gitember.GitemberLauncher   --type "dmg"  --icon src/main/resources/icon/gitember.ico
+
+mv Gitember2-1.0.dmg Gitember2.dmg
+
+
+#export PATH="/Users/ec2-user/jdk-16.0.1.jdk/Contents/Home/bin:/Users/ec2-user/apache-maven-3.8.1/bin:$PATH"
+
+
