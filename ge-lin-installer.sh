183565871d3be2b40c924749c31e21967c25ce8c
Igor Azarny <iazarny@yahoo.com>
2021-06-14 18:13:52 +0000

    Linux build deb

---
+++ ge-lin-installer.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+jpackage --input shade/  --name Gitember2 --vendor "Igor Azarny"  --main-jar gitember.jar  --main-class com.az.gitember.GitemberLauncher   --type "deb"  --icon src/main/resources/icon/gitember.ico
+
+mv Gitember2-1.0.deb Gitember2.deb
+
+
+
+
