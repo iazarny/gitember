2e70625ac720702980c5f8c8f579af504c0922d0
Igor Azarny <iazarny@yahoo.com>
2021-06-14 19:31:10 +0000

    Bild script

--- ge-lin-installer.sh
+++ ge-lin-installer.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-jpackage --input shade/  --name Gitember2 --vendor "Igor Azarny"  --main-jar gitember.jar  --main-class com.az.gitember.GitemberLauncher   --type "deb"  --icon src/main/resources/icon/gitember.ico
-
+jpackage --input shade/  --name Gitember2 --vendor "Igor Azarny"  --main-jar gitember.jar  --main-class com.az.gitember.GitemberLauncher   --type "deb"  --icon src/main/resources/icon/gitember.png
 mv Gitember2-1.0.deb Gitember2.deb
 
 
