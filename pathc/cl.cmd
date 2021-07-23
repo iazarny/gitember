07efbde19981f22dc5e829719b43f48a9171b9ff
Igor Azarny <iazarny@yahoo.com>
2021-07-23 08:55:32 +0300

    Cleanup

--- pathc/cl.cmd
+++
@@ -1,3 +0,0 @@
-jdeps --add-modules --generate-module-info . C:\Users\Igor_Azarny\.m2\repository\commons-io\commons-io\2.8.0\commons-io-2.8.0.jar
-jdeps --add-modules --generate-module-info . C:\Users\Igor_Azarny\.m2\repository\org\eclipse\jgit\org.eclipse.jgit\5.3.9.202012012026-r\org.eclipse.jgit-5.3.9.202012012026-r.jar
-jdeps --add-modules --generate-module-info . C:/Users/Igor_Azarny/.m2/repository/org/apache/commons/commons-lang3/3.12.0/commons-lang3-3.12.0.jar
