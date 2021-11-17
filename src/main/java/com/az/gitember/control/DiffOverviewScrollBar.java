612e84997711dc408a96e2b795fbfdd160d1a0a6
Azarny, Igor <iazarny@yahoo.com>
2021-11-17 10:47:38 +0200

    Diff iverview scroll bar

--- src/main/java/com/az/gitember/control/DiffOverviewScrollBar.java
+++ src/main/java/com/az/gitember/control/DiffOverviewScrollBar.java
@@ -1,2 +1,19 @@
-package com.az.gitember.control;public class DiffOverviewScrollBar {
+package com.az.gitember.control;
+
+
+import javafx.scene.control.ScrollBar;
+
+/**
+ * By Igor Azarny iazarny@yahoo.com 14-Nov-2021
+ */
+public class DiffOverviewScrollBar extends ScrollBar {
+
+    public DiffOverviewScrollBar() {
+        super();
+    }
+
+    @Override
+    protected void layoutChildren() {
+        super.layoutChildren();
+    }
 }
