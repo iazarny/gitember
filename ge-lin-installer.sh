f4a10a139d0dc16d7ae9bcee439239a9bb6feb10
Igor Azarny <iazarny@yahoo.com>
2021-12-05 14:25:58 +0200

    Update shell scripts to build installers

--- ge-lin-installer.sh
+++ ge-lin-installer.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
+
 jpackage --input shade/  --name Gitember2 --vendor "Igor Azarny"  --main-jar gitember.jar  --main-class com.az.gitember.GitemberLauncher   --type "deb"  --icon src/main/resources/icon/gitember.png
-mv Gitember2-1.0.deb Gitember2.deb
 
-
+mv gitember2_1.0-1_amd64.deb Gitember2.deb
 
 
