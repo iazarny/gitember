554ec69f03c1f648d0e039df14d3f7b5789f77b2
Azarny, Igor <iazarny@yahoo.com>
2022-02-04 17:01:01 +0200

    Gitlab issues in progress. Notes

--- src/main/java/com/az/gitember/gitlab/CommentView.java
+++ src/main/java/com/az/gitember/gitlab/CommentView.java
@@ -7,21 +7,26 @@ import com.az.gitember.gitlab.model.FxIssue;
 import com.az.gitember.gitlab.model.GitLabProject;
 import com.az.gitember.service.Context;
 import com.az.gitember.service.GitemberUtil;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Hyperlink;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
+import javafx.scene.control.*;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
 import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.gitlab4j.api.GitLabApiException;
 import org.gitlab4j.api.models.Note;
+import org.kordamp.ikonli.fontawesome.FontAwesome;
+import org.kordamp.ikonli.javafx.FontIcon;
+import org.kordamp.ikonli.javafx.StackedFontIcon;
 
 public class CommentView extends VBox {
 
     private final String SPACES = "   ";
     public CommentView(final Note note, final boolean parent) {
+
+        if (parent) {
+            this.getChildren().add(new HBox(new Label(SPACES)));
+        }
         HBox head = new HBox();
 
         head.getChildren().add(new Label(SPACES));
@@ -34,7 +39,8 @@ public class CommentView extends VBox {
         Region region = new Region();
         HBox.setHgrow(region, Priority.ALWAYS);
         head.getChildren().add(region);
-        head.getChildren().add(new Label("X"));
+        addReplyButton(head, parent);
+        addEditButton(head, note);
         head.getChildren().add(new Label(SPACES));
 
         TextArea comment = new TextArea(note.getBody());
@@ -56,6 +62,36 @@ public class CommentView extends VBox {
         this.setStyle("-fx-border-width : 1 ; ");
 
 
+    }
+
+    private void addEditButton(HBox head, final Note note) {
+        String currentUserName = null;
+        try {
+            currentUserName = Context.getGitLabProject().getGitLabApi().getUserApi().getCurrentUser().getUsername();
+            if (note.getAuthor().getUsername().trim().equals(currentUserName)) {
+                Button editBtn = new Button("Edit");
+                StackedFontIcon replyIcon = new StackedFontIcon();
+                replyIcon.getChildren().add(new FontIcon(FontAwesome.EDIT));
+                replyIcon.setStyle("-fx-icon-color: text_color");
+                editBtn.setGraphic(replyIcon);
+                head.getChildren().add(editBtn);
+            }
+        } catch (GitLabApiException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    private void addReplyButton(HBox head, boolean parent) {
+        if (parent) {
+            Button replyBtn = new Button("Reply");
+            StackedFontIcon replyIcon = new StackedFontIcon();
+            replyIcon.getChildren().add(new FontIcon(FontAwesome.REPLY));
+            replyIcon.setStyle("-fx-icon-color: text_color");
+            replyBtn.setGraphic(replyIcon);
+            head.getChildren().add(replyBtn);
+        }
     }
 
 
