2ed4b119db55f3e91fe60267853987d710c3e5cc
Igor Azarny <iazarny@yahoo.com>
2024-03-02 15:07:53 +0300

    Win installer

--- ge-win-installer.cmd
+++ ge-win-installer.cmd
@@ -1,2 +1,7 @@
-jpackage --input shade/  --name Gitember2 --vendor "Igor Azarny"  --main-jar gitember.jar  --main-class com.az.gitember.GitemberLauncher --win-menu --win-shortcut --type "msi"  --icon src\main\resources\icon\gitember.ico 
-move Gitember2-1.0.msi Gitember2.msi
+mkdir app
+
+move target\gitember-2.5-spring-boot.jar app
+
+jpackage --input app/  --name Gitember2 --vendor "Igor Azarny"  --main-jar gitember-2.5-spring-boot.jar  --win-menu --win-shortcut --type "msi"  --icon src\main\resources\icon\gitember.ico
+
+move Gitember2-1.0.msi Gitember2.5.msi
