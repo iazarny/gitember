e6af843e0f220fb7e2a307f2c1983a6d381c76f7
Michal Hečko <michal.hecko@icz.sk>
2024-05-08 09:29:40 +0200

    Initial modularization of HistoryCommits view

--- src/main/java/com/az/gitember/module/history/HistoryController.java
+++ src/main/java/com/az/gitember/module/history/HistoryController.java
@@ -1,31 +1,31 @@
-package com.az.gitember.controller;
+package com.az.gitember.module.history;
 
 import com.az.gitember.App;
 import com.az.gitember.control.ChangeListenerHistoryHint;
 import com.az.gitember.controller.handlers.CheckoutRevisionhEventHandler;
 import com.az.gitember.data.Const;
-import com.az.gitember.data.ScmItem;
 import com.az.gitember.data.Settings;
+import com.az.gitember.module.history.factory.HistoryCommitsTableLaneCellFactory;
+import com.az.gitember.module.history.factory.HistoryCommitsTableRowFactory;
+import com.az.gitember.module.history.listener.HistoryCommitsTableViewChangeListener;
+import com.az.gitember.module.history.renderer.HistoryPlotCommitRenderer;
 import com.az.gitember.service.Context;
 import com.az.gitember.service.GitemberUtil;
 import javafx.beans.InvalidationListener;
 import javafx.beans.property.ReadOnlyStringWrapper;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
-import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
 import org.apache.commons.lang3.exception.ExceptionUtils;
-import org.eclipse.jgit.api.CheckoutCommand;
 import org.eclipse.jgit.api.CherryPickResult;
-import org.eclipse.jgit.lib.PersonIdent;
 import org.eclipse.jgit.revplot.PlotCommit;
+import org.eclipse.jgit.revplot.PlotLane;
 import org.eclipse.jgit.revwalk.RevCommit;
 
 import java.io.IOException;
@@ -33,20 +33,14 @@ import java.net.URL;
 import java.util.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.stream.Collectors;
 
-public class History implements Initializable {
-
-    private final static Logger log = Logger.getLogger(History.class.getName());
+public class HistoryController implements Initializable {
+    private final static Logger log = Logger.getLogger(HistoryController.class.getName());
 
     private static final int HEIGH = 40;
 
-
-    private int plotWidth = 20 * HEIGH;
-
     @FXML
     public AnchorPane hostHistoryViewPanel;
-    public AnchorPane hostCommitViewPanel;
     public SplitPane splitPanel;
     public BorderPane mainBorderPanel;
     public Pane spacerPane;
@@ -59,159 +53,44 @@ public class History implements Initializable {
     private TextField searchText;
 
     @FXML
-    private TableColumn<PlotCommit, Canvas> laneTableColumn;
+    private TableColumn<PlotCommit<PlotLane>, Canvas> laneTableColumn;
 
     @FXML
-    private TableColumn<PlotCommit, String> dateTableColumn;
+    private TableColumn<PlotCommit<PlotLane>, String> dateTableColumn;
 
     @FXML
-    private TableColumn<PlotCommit, String> messageTableColumn;
+    private TableColumn<PlotCommit<PlotLane>, String> authorTableColumn;
 
     @FXML
-    private TableColumn<PlotCommit, String> authorTableColumn;
-
-    @FXML
-    public TableColumn<PlotCommit, String> shaTableColumn;
-
-    @FXML
-    public TableColumn<PlotCommit, String> refTableColumn;
+    public TableColumn<PlotCommit<PlotLane>, String> shaTableColumn;
 
     @FXML
     private TableView commitsTableView;
 
 
-
-
-
     @Override
     @SuppressWarnings("unchecked")
     public void initialize(URL location, ResourceBundle resources) {
-
-
         commitsTableView.setFixedCellSize(HEIGH);
         commitsTableView
                 .getSelectionModel()
                 .selectedItemProperty()
-                .addListener(new ChangeListener<PlotCommit>() {
+                .addListener(new HistoryCommitsTableViewChangeListener(mainBorderPanel, splitPanel));
 
-                    @Override
-                    public void changed(final ObservableValue<? extends PlotCommit> observable,
-                                        final PlotCommit oldValue,
-                                        final PlotCommit newValue) {
+        commitsTableView.setRowFactory(tableView -> new HistoryCommitsTableRowFactory(searchText));
 
-                        if (splitPanel.getItems().size() == 1) {
-                            hostCommitViewPanel = new AnchorPane();
-                            hostCommitViewPanel.prefHeight(330);
-                            hostCommitViewPanel.minHeight(250);
-                            splitPanel.getItems().add(hostCommitViewPanel);
-                            splitPanel.setDividerPositions(0.65);
-                            mainBorderPanel.layout();
-                        }
-                        if (newValue != null) {
-                            try {
-                                Context.scmRevCommitDetails.setValue(Context.getGitRepoService().adapt(newValue));
-                                final Parent commitView = App.loadFXML(Const.View.HISTORY_DETAIL).getFirst();
-                                hostCommitViewPanel.getChildren().clear();
-                                hostCommitViewPanel.getChildren().add(commitView);
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                            }
-                        }
-
-                    }
-
-                });
-
-
-
-        commitsTableView.setRowFactory(
-                tr -> {
-                    return new TableRow<PlotCommit>() {
-                        private String calculateStyle(final PlotCommit plotCommit) {
-                            String searchString = searchText.getText();
-                            if (isSearchEligible(plotCommit, searchString)) {
-                                Map<String, Set<String>> map = Context.searchResult.getValue();
-                                if (map != null && map.containsKey(plotCommit.getName()) ) {
-                                    return LookAndFeelSet.FOUND_ROW;
-                                }
-                            }
-                            return "";
-                        }
-
-                        @Override
-                        protected void updateItem(PlotCommit item, boolean empty) {
-                            super.updateItem(item, empty);
-                            setStyle(calculateStyle(item));
-                        }
-
-                        private boolean isSearchEligible(PlotCommit plotCommit, String searchString) {
-                            return plotCommit != null
-                                    && plotCommit.getName() != null
-                                    && searchString != null
-                                    && searchString.length() > Const.SEARCH_LIMIT_CHAR;
-                        }
-                    };
-                }
-        );
-
-        laneTableColumn.setCellValueFactory(
-                c -> {
-                    return new ObservableValue<Canvas>() {
-                        @Override
-                        public Canvas getValue() {
-                            Canvas canvas = new Canvas(plotWidth, HEIGH);
-                            GraphicsContext gc = canvas.getGraphicsContext2D();
-                            plotCommitRenderer.render(gc, c.getValue(), HEIGH);
-                            return canvas;
-                        }
-
-                        @Override
-                        public void addListener(InvalidationListener listener) {
-                        }
-
-                        @Override
-                        public void removeListener(InvalidationListener listener) {
-                        }
-
-                        @Override
-                        public void addListener(ChangeListener<? super Canvas> listener) {
-                        }
-
-                        @Override
-                        public void removeListener(ChangeListener<? super Canvas> listener) {
-                        }
-
-                    };
-                }
-        );
+        commitsTableView.widthProperty().addListener((observable, oldValue, newValue) -> {
+            laneTableColumn.setPrefWidth(newValue.doubleValue() - authorTableColumn.getWidth() - dateTableColumn.getWidth() - shaTableColumn.getWidth() - 20);
+        });
+        laneTableColumn.setCellValueFactory(c -> new HistoryCommitsTableLaneCellFactory(c.getValue(), plotCommitRenderer, HEIGH));
 
         shaTableColumn.setCellValueFactory(
                 c -> new ReadOnlyStringWrapper(c.getValue().getName())
         );
 
-        refTableColumn.setCellValueFactory(
-                c -> {
-                    PlotCommit plotCommit = c.getValue();
-                    LinkedList<String> refs = new LinkedList<>();
-                    for (int i = 0; i < plotCommit.getRefCount(); i++) {
-                        refs.add(
-                                plotCommit.getRef(i).getName()
-                        );
-                    }
-                    return new ReadOnlyStringWrapper(refs.stream().collect(Collectors.joining(", ")));
-                }
-        );
-
         authorTableColumn.setCellValueFactory(
                 c -> new ReadOnlyStringWrapper(c.getValue().getAuthorIdent().getName())
         );
-
-        messageTableColumn.setCellValueFactory(
-                c ->  {
-                    return new ReadOnlyStringWrapper( c.getValue().getShortMessage().replace("\n","") );
-                }
-        );
-
 
         dateTableColumn.setCellValueFactory(
                 c -> new ReadOnlyStringWrapper(
@@ -259,11 +138,7 @@ public class History implements Initializable {
 
         HBox.setHgrow(spacerPane, Priority.ALWAYS);
         commitsTableView.setItems(Context.plotCommitList);
-
     }
-
-
-
 
     public void checkoutMenuItemClickHandler(ActionEvent actionEvent) {
 
