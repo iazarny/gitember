6af6a02de85af5b9613ff81cef1b5db63776fab7
Igor Azarny <iazarny@yahoo.com>
2026-03-10 21:05:22 +0300

    Win installer

--- ge-win-installer.cmd
+++ ge-win-installer.cmd
@@ -2,6 +2,6 @@ rem mkdir app
 
 rem move target\gitember-3.0-SNAPSHOT-boot.jar app
 
-jpackage --input app/  --name Gitember --app-version 3.0  --vendor "Igor Azarny"  --main-jar gitember-3.0-SNAPSHOT-boot.jar  --win-menu --win-shortcut --type "msi"  --icon src\main\resources\icon\gitember.ico
+jpackage --input app/  --name Gitember --win-menu-group Gitember --install-dir gitember --app-version 3.0  --vendor "Igor Azarny"  --main-jar gitember-3.0-SNAPSHOT-boot.jar  --win-menu --win-shortcut --type "msi"  --icon src\main\resources\icon\gitember.ico
 
 rem move Gitember2-1.0.msi Gitember2.5.msi
